8d2c0396e67d0020b1c2aced1ba6107f6fa45bfd
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-11 11:51:46 +0530

    HotelManager model created in owner model

--- owner/models.py
+++ owner/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class HotelManager(models.Model):
+    hotel_name = models.CharField(max_length=100,blank=False)
+    location = models.CharField(max_length=500)
+    pincode = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.hotel_name
