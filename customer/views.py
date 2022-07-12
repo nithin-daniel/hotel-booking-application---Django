c13e8aa4cc839202e0c39a6b362b60d087930ee7
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-12 22:26:51 +0530

    login and register template setuped

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'customer/login.html')
+
+def register(request):
+    return render(request, 'customer/register.html')
--- hotelBokking/urls.py
+++ hotelBokking/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('owner/',include('owner.urls')),
+    path('customer/',include('customer.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
