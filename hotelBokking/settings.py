79dbc6468eceb299aed61821bd3659352585141b
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-12 07:59:54 +0530

    static file configured

--- hotelBokking/settings.py
+++ hotelBokking/settings.py
@@ -127,6 +127,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
