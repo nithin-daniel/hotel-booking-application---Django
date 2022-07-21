4d39a490b9a101ee39e9b2c05e0a188bcd098a34
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-21 11:15:38 +0530

    template changes done

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
 # Create your views here.
 def login(request):
     return render(request, 'customer/login.html')
 
 def register(request):
+
     return render(request, 'customer/register.html')
+    
