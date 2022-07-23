a7453959901363308ef7099bdf139a3a2ca27cd2
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-23 11:07:51 +0530

    registeration forms.py help_text removed

--- customer/forms.py
+++ customer/forms.py
@@ -6,13 +6,19 @@ from crispy_forms.layout import Submit
 
 class RegisterForm(UserCreationForm):
 	email = forms.EmailField()
-
+	password1 = forms.CharField(help_text=None,widget=forms.PasswordInput())
+	# def _init_(self, *args, **kwargs):
+	# 	super(UserCreateForm, self)._init_(*args, **kwargs)
+	# 	for fieldname in ['username', 'password1', 'password2']:
+	# 		self.fields[fieldname].help_text = None 
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
+		labels = {
+            'password1': 'Password',
+        }
+		help_texts = {
+            'username': None,
+            'email': None,
+        }
 
-	# def _init_(self, *args, **kwargs):
-	# 	super(UserCreateForm, self)._init_(*args, **kwargs)
-
-	# 	for fieldname in ['username', 'password1', 'password2']:
-    #     	self.fields[fieldname].help_text = None
